412d77013d2bc0d9ad013e432ee0a70956d6d0bb
--global <metal_bullets@filmsbykris.com>
2014-04-29 15:17:50 -0400

    Updated the navigation bar for all forms. they now have a home button.

--- station_supply/index.php
+++ station_supply/index.php
@@ -3,20 +3,27 @@
 	<head>
                 <?php include("../headers/header1.php");?>
 		<script src="../js/supply_submit.js"></script>
+                <script>
+                    $(document).ready(function(){
+                        //set title
+                        $("#title").html("Station Supply Order Form");
+                    });
+                </script>
 	</head>
 	<body>
 		<div data-role="page" data-theme="a">
-			<div data-role="header" data-position="inline">
-				<h1>Station Supply Order</h1>
-			</div>
+                        <?php include("../php/nav_home.php");?>
+
 			<div data-role="content" data-theme="a">
 				<?php 
 					include("../employee/user.php");
 					include("../stations/station.php");
 
 				?>
+
                                 <label>Quantity</label>
                                 <input type="number" id="quantity" name="quantity" value="1">
+
 
                                 <ul data-role="listview" data-filter="true" data-inset="true">
                                     <?php include("get_list.php");?>
